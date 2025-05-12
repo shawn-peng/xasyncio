ceee4ed6df84ecdd8b7b31fed7d9c2606a6a428d
Yisu Peng <yisupeng@gmail.com>
2025-05-11 22:45:37 -0700

    fix bug

--- src/asyncio_utils/__init__.py
+++ src/asyncio_utils/__init__.py
@@ -3,24 +3,56 @@ import threading
 
 
 class ThreadedEventLoop:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.thread = threading.Thread(target=self.run)
-        self.thread.start()
         self.events = {}
         self.events_out_thread = {}
         self.loop = None
+        self.stopped = True
+        self.thread.start()
         self.create_out_thread_event('loop_started')
-        print('waiting for loop to start')
         self.wait_out_thread_event('loop_started')
-        print('Threaded Loop is ready for using')
+
+    def _stop(self):
+        """this function must be called with the thread"""
+        if self.stopped:
+            return
+        self.loop.stop()
+        self.stopped = True
+
+    def stop(self):
+        """thread safe stop function"""
+        # thread = threading.current_thread()
+        # if thread == self.thread:
+        #     print('calling from the loop thread')
+        # else:
+        #     print('calling from another loop')
+        self.call_sync(self._stop)
+        self.thread.join(10)
+
+    def _mark_running(self, running=True):
+        # if running:
+        #     print('mark running')
+        # else:
+        #     print('mark stopped')
+        self.stopped = not running
 
     def run(self):
         self.loop = asyncio.new_event_loop()
         # self.loop = asyncio.get_event_loop()
         # Need to call this in the loop, mainly because need to make sure the loop is running
+        # self.loop.call_soon_threadsafe(
+        #     lambda: (
+        #         print('notifying loop started'), self._mark_running(), print(self.stopped), self.notify_out_thread_event('loop_started')))
         self.loop.call_soon_threadsafe(
-            lambda: (print('notifying loop started'), self.notify_out_thread_event('loop_started')))
+            lambda: (
+                self._mark_running(), self.notify_out_thread_event('loop_started')
+            )
+        )
+
         self.loop.run_forever()
+        print(f'Loop ({self.name}) finished')
 
     def create_out_thread_event(self, name):
         self.events_out_thread[name] = threading.Event()
@@ -29,6 +61,7 @@ class ThreadedEventLoop:
         self.events_out_thread[name].wait()
 
     def notify_out_thread_event(self, name):
+        print('notify event', name)
         self.events_out_thread[name].set()
 
     def create_event(self, name):
@@ -40,8 +73,11 @@ class ThreadedEventLoop:
     def wait(self, event_name):
         self.events[event_name].wait()
 
-    def call(self, func, *args):
+    def call_sync(self, func, *args):
         blocking_call_w_loop(self.loop, func, *args)
+
+    def call_async(self, func, *args):
+        self.loop.call_soon_threadsafe(func, *args)
 
 
 def blocking_call_w_loop(loop, func, *args):
